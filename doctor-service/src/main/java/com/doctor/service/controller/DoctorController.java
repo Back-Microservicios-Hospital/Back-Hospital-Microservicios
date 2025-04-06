3eb6394161baa0fab7d89ac7694850e764bc559d
Tu Nombre <luis.orihuela@upeu.edu.pe>
2025-04-06 16:56:14 -0500

    agregando busqueda de doctor por apellido en el controller y graphql

--- doctor-service/src/main/java/com/doctor/service/controller/DoctorController.java
+++ doctor-service/src/main/java/com/doctor/service/controller/DoctorController.java
@@ -31,178 +31,200 @@ import com.doctor.service.service.EspecialidadService;
 public class DoctorController {
 
 	private static Logger logger = LoggerFactory.getLogger(DoctorController.class);
-	
+
 	@Autowired
 	private DoctorService doctorService;
-	
+
 	@Autowired
 	private EspecialidadService especialidadService;
-	
+
 	@GetMapping("/list/all")
-	public ResponseEntity<?> listAllDoctors(){
-		
+	public ResponseEntity<?> listAllDoctors() {
+
 		try {
-			
+
 			List<Doctor> listDoctors = doctorService.findAllDoctors();
-			
+
 			logger.info("Listado de Doctores OK !");
 			return new ResponseEntity<>(listDoctors, HttpStatus.OK);
-			
+
 		} catch (Exception e) {
 			logger.error("Error al listar todos los doctores {}", e);
-			return new ResponseEntity<>(Map.of("error", "Error al listar todos los doctores",
-											   "detalle", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(
+					Map.of("error", "Error al listar todos los doctores", "detalle", e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	//Listar doctores con paginacion
+
+	// Listar doctores con paginacion
 	@GetMapping("/list/pagination")
-	public ResponseEntity<?> listDoctorsByPagination(@RequestParam int page,
-													 @RequestParam int size){
-		
+	public ResponseEntity<?> listDoctorsByPagination(@RequestParam int page, @RequestParam int size) {
+
 		try {
-			
+
 			Page<Doctor> listPagination = doctorService.findDoctorByPagination(page, size);
-			
+
 			logger.info("Listado de doctores con paginación OK !");
 			return new ResponseEntity<>(listPagination, HttpStatus.OK);
-			
+
 		} catch (Exception e) {
 			logger.error("Error al listar todos los doctores con paginación {}", e);
-			return new ResponseEntity<>(Map.of("error", "Error al listar todos los doctores con paginación",
-											   "detalle", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(
+					Map.of("error", "Error al listar todos los doctores con paginación", "detalle", e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+
 	}
-	
-	//Buscar un doctor por su id
+
+	// Buscar un doctor por su id
 	@GetMapping("/find/{id}")
-	public ResponseEntity<?> findDoctorById(@PathVariable Long id){
-		
+	public ResponseEntity<?> findDoctorById(@PathVariable Long id) {
+
 		try {
-			
+
 			Optional<Doctor> doctorOptional = doctorService.findDoctorById(id);
-			
+
 			if (doctorOptional.isEmpty()) {
 				logger.error("Error, doctor no encontrado con el id: {}", id);
 				throw new RuntimeException("Error, doctor no encontrado con el id: " + id);
 			}
-			
+
 			logger.info("Doctor encontrado: {}", doctorOptional);
 			return new ResponseEntity<>(doctorOptional, HttpStatus.OK);
-			
+
 		} catch (Exception e) {
 			logger.error("Error, al encontrado el Doctor {}", e);
-			return new ResponseEntity<>(Map.of("error", "Error al encontrar el doctor", 
-												"detalle", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(Map.of("error", "Error al encontrar el doctor", "detalle", e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	@PostMapping("/create")
-	public ResponseEntity<?> createDoctor(@RequestBody DoctorDTO doctorDTO){
-		
+
+	// Buscar doctor por apellido
+	@GetMapping("/find/apellido/{apellido}")
+	public ResponseEntity<?> getDoctorByApellido(@PathVariable String apellido) {
+
 		try {
-			
+
+			List<Doctor> listPaciente = doctorService.findDoctorByApellido(apellido);
+
+			if (listPaciente.isEmpty()) {
+				logger.error("Error, no se encontro doctor con el apellido: {}", apellido);
+				throw new RuntimeException("Error, no se obtuvo al doctor con el apellido: " + apellido);
+			}
+
+			logger.info("Doctor encontrado con el apellido ingresado: {}", listPaciente);
+			return new ResponseEntity<>(listPaciente, HttpStatus.OK);
+
+		} catch (Exception e) {
+			logger.error("Error, al encontrado el Doctor por el apellido {}", e);
+			return new ResponseEntity<>(
+					Map.of("error", "Error al encontrar el doctor por el apellido", "detalle", e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<?> createDoctor(@RequestBody DoctorDTO doctorDTO) {
+
+		try {
+
 			Doctor doctor = new Doctor();
 			doctor.setNombre(doctorDTO.getNombre());
 			doctor.setApellido(doctorDTO.getApellido());
 			doctor.setTelefono(doctorDTO.getTelefono());
 			doctor.setEmail(doctorDTO.getEmail());
-			
-			Optional<Especialidad> especialidadOptional = especialidadService.findEspecialidadById(doctorDTO.getEspecialidadId());
-			
+
+			Optional<Especialidad> especialidadOptional = especialidadService
+					.findEspecialidadById(doctorDTO.getEspecialidadId());
+
 			if (especialidadOptional.isEmpty()) {
 				logger.error("Error, especialidad no encontrada con el id: ", doctorDTO.getEspecialidadId());
-				throw new RuntimeException("Error, Especialidad no encontrada con el id : " + doctorDTO.getEspecialidadId());
+				throw new RuntimeException(
+						"Error, Especialidad no encontrada con el id : " + doctorDTO.getEspecialidadId());
 			}
-			
+
 			Especialidad especialidad = especialidadOptional.get();
-			
+
 			doctor.setEspecialidad(especialidad);
-			
+
 			doctorService.saveDoctor(doctor);
-			
+
 			logger.info("Doctor creado: {}", doctor);
 			return new ResponseEntity<>(doctor, HttpStatus.CREATED);
-			
+
 		} catch (Exception e) {
 			logger.error("Error al crear el doctor {}", e);
-			return new ResponseEntity<>(Map.of("error", "Error al crear el doctor",
-											   "detalle", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(Map.of("error", "Error al crear el doctor", "detalle", e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
+
 	@PutMapping("/update/{id}")
-	public ResponseEntity<?> updateDoctor (@PathVariable Long id,
-										   @RequestBody DoctorDTO doctorDTO){
-		
+	public ResponseEntity<?> updateDoctor(@PathVariable Long id, @RequestBody DoctorDTO doctorDTO) {
+
 		try {
-			
+
 			Optional<Doctor> doctorOptional = doctorService.findDoctorById(id);
-			
+
 			if (doctorOptional.isEmpty()) {
 				logger.error("Error, doctor no encontrado con el id: {}", id);
 				throw new RuntimeException("Error, doctor no encontrado con el id: " + id);
 			}
-			
+
 			Doctor doctor = doctorOptional.get();
 			doctor.setNombre(doctorDTO.getNombre());
 			doctor.setApellido(doctorDTO.getApellido());
 			doctor.setTelefono(doctorDTO.getTelefono());
 			doctor.setEmail(doctorDTO.getEmail());
-			
-			Optional<Especialidad> especialidadOptional = especialidadService.findEspecialidadById(doctorDTO.getEspecialidadId());
-			
+
+			Optional<Especialidad> especialidadOptional = especialidadService
+					.findEspecialidadById(doctorDTO.getEspecialidadId());
+
 			if (especialidadOptional.isEmpty()) {
 				logger.error("Error, especialidad no encontrada con el id: ", doctorDTO.getEspecialidadId());
-				throw new RuntimeException("Error, Especialidad no encontrada con el id : " + doctorDTO.getEspecialidadId());
+				throw new RuntimeException(
+						"Error, Especialidad no encontrada con el id : " + doctorDTO.getEspecialidadId());
 			}
-			
+
 			Especialidad especialidad = especialidadOptional.get();
-			
+
 			doctor.setEspecialidad(especialidad);
-			
+
 			doctorService.saveDoctor(doctor);
-			
+
 			logger.info("Doctor actualizado con éxito: {}", doctor);
 			return new ResponseEntity<>(doctor, HttpStatus.OK);
-			
+
 		} catch (Exception e) {
 			logger.error("Error al actualizar el doctor {}", e);
-			return new ResponseEntity<>(Map.of("error", "Error al actualizar el doctor",
-											   "detalle", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(Map.of("error", "Error al actualizar el doctor", "detalle", e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+
 	}
-	
+
 	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<?> deleteDoctor (@PathVariable Long id){
-		
+	public ResponseEntity<?> deleteDoctor(@PathVariable Long id) {
+
 		try {
-			
+
 			Optional<Doctor> doctorOptional = doctorService.findDoctorById(id);
-			
+
 			if (doctorOptional.isEmpty()) {
 				logger.error("Error, doctor no encontrado con el id: {}", id);
 				throw new RuntimeException("Error, doctor no encontrado con el id: " + id);
 			}
-			
+
 			doctorService.deleteDoctor(id);
-			
+
 			logger.info("Doctor eliminado con éxito !");
 			return new ResponseEntity<>(Map.of("mensaje", "Doctor eliminado con éxito"), HttpStatus.OK);
-					
+
 		} catch (Exception e) {
 			logger.error("Error al eliminar el doctor {}", e);
-			return new ResponseEntity<>(Map.of("error", "Error al eliminar el doctor",
-											   "detalle", e.getMessage()), HttpStatus.INTERNAL_SERVER_ERROR);
+			return new ResponseEntity<>(Map.of("error", "Error al eliminar el doctor", "detalle", e.getMessage()),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
+
 }
--- doctor-service/src/main/java/com/doctor/service/graphql/GraphQLController.java
+++ doctor-service/src/main/java/com/doctor/service/graphql/GraphQLController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -32,6 +33,28 @@ public class GraphQLController {
 		} catch (Exception e) {
 			logger.error("Error al listar los doctores con GraphQL: {}", e);
 			throw new RuntimeException("Error al listar los doctores con GraphQL " + e.getMessage());
+		}
+	}
+	
+	//Buscar doctor por apellido
+	@QueryMapping(name = "findDoctorByApellido")
+	public List<Doctor> getDoctorByApellido(@Argument(name = "apellido") String apellido){
+		
+		try {
+			
+			List<Doctor> listDoctor = doctorService.findDoctorByApellido(apellido);
+			
+			if (listDoctor.isEmpty()) {
+				logger.error("Error, doctor no encontrado con el apellido: {}", apellido);
+				throw new RuntimeException("Error, no se obtuvo resultado de búsqueda de doctor con el apellido: " + apellido);
+			}
+			
+			logger.info("Doctor encontrado con el apellido ingresado: {}", listDoctor);
+			return listDoctor;
+			
+		} catch (Exception e) {
+			logger.error("Error al encontrar doctor por el apellido con GraphQL: {}", e);
+			throw new RuntimeException("Error al encontrar doctor por el apellido con GraphQL " + e.getMessage());
 		}
 	}
 }
